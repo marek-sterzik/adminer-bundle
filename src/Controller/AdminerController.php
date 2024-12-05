908b3a3d5d4b8fee08fcfbf1bc798ed2df6cf9ec
Marek Sterzik <marek.sterzik@gmail.com>
2024-12-05 22:19:00 +0100

    initial import

---
+++ src/Controller/AdminerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sterzik\AdminerBundle\Controller;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class AdminerController extends AbstractController
+{
+    public function __construct(
+        private ?string $databaseUrl = null,
+    ) {
+    }
+
+    #[Route("/adminer", name: "adminer_bundle.adminer")]
+    public function pageAdminer(): Response
+    {
+        $bootFile = dirname(__DIR__, 2) . "/adminer/boot.php";
+        return new StreamedResponse(
+            function () {
+                $httpAuth = null;
+                $dbConf = $this->databaseUrl;
+                include $bootFile;
+            }
+        );
+    }
+}
